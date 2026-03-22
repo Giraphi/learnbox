132faac685d1a2bb1d5d744fdf327270cf9c2a4d
RaphaelHoepsWhy <raphael.hoeps@why.de>
2026-03-22 19:07:05 +0100

    feat: prepopulate

--- app/db.ts
+++ app/db.ts
@@ -14,5 +14,14 @@ db.version(1).stores({
   vocabularies: "id",
 });
 
+db.on("populate", (transaction) => {
+  transaction.table("vocabularies").bulkAdd([
+    { id: "1", english: "stroller", german: "Kinderwagen" },
+    { id: "2", english: "reckless", german: "rücksichtslos" },
+    { id: "3", english: "irresponsible", german: "unverantwortlich" },
+    { id: "4", english: "deliberate", german: "absichtlich" },
+  ]);
+});
+
 export { db };
 export type { Vocabulary };
