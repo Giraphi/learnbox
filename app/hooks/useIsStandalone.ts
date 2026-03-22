bc9572539937fbbb9fae48cdf6aa967908ba96a7
RaphaelHoepsWhy <raphael.hoeps@why.de>
2026-03-22 17:53:26 +0100

    feat: pwa baseline

--- app/hooks/useIsStandalone.ts
+++ app/hooks/useIsStandalone.ts
@@ -8,6 +8,6 @@ export function useIsStandalone() {
   return useSyncExternalStore(
     EMPTY_SUBSCRIBE,
     () => window.matchMedia("(display-mode: standalone)").matches,
-    () => false
+    () => true
   );
 }
