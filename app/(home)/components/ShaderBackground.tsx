361f7fe2472196fa6de73bac4baf38a1ed0fa6ad
RaphaelHoepsWhy <raphael.hoeps@why.de>
2026-04-21 10:46:43 +0200

    feat: background

--- app/(home)/components/ShaderBackground.tsx
+++ app/(home)/components/ShaderBackground.tsx
@@ -4,21 +4,23 @@ import { ShaderGradientCanvas, ShaderGradient } from "@shadergradient/react";
 export default function ShaderBackground() {
   return (
     <ShaderGradientCanvas
-      style={{ position: "absolute", inset: 0, pointerEvents: "none" }}
+      style={{
+        position: "absolute",
+        inset: 0,
+        pointerEvents: "none",
+      }}
       pixelDensity={1.5}
-      fov={45}
+      fov={30}
     >
       <ShaderGradient
-        cDistance={6}
+        cDistance={12}
         cPolarAngle={90}
         animate="on"
-        uSpeed={0.1}
-        // color1="#f4a672"
-        // color2="#f28bb0"
-        // color3="#ffd98a"
-        // brightness={0.9}
-        grain="on"
-        grainBlending={0.00001}
+        uSpeed={0.5}
+        color1="#aaa"
+        color2="#111"
+        brightness={0.8}
+        envPreset="dawn"
       />
     </ShaderGradientCanvas>
   );
